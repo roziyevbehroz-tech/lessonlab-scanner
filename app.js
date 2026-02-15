92f2882dd8ad4e043eadb976f6af53aa8bf0060f
Ruziyev Behruz <roziyevbehroz@mail.com>
2026-02-15 14:19:45 +0500

    v9.1: PeerJS debug and more STUN servers

--- app.js
+++ app.js
@@ -92,11 +92,14 @@
 
         try {
             syncPeer = new Peer(scannerId, {
-                debug: 0,
+                debug: 3,
                 config: {
                     iceServers: [
                         { urls: 'stun:stun.l.google.com:19302' },
                         { urls: 'stun:stun1.l.google.com:19302' },
+                        { urls: 'stun:stun2.l.google.com:19302' },
+                        { urls: 'stun:stun3.l.google.com:19302' },
+                        { urls: 'stun:stun4.l.google.com:19302' },
                         { urls: 'stun:global.stun.twilio.com:3478' }
                     ]
                 }
